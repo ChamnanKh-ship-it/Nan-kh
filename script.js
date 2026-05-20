7abd2e5e72e8ea8f43cef8a0b1e4621577bc8a46
Nan Kh <lonjamnan050@gmail.com>
2026-05-21 04:01:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("button");
+
+btn.onclick = (e) => {
+  let x = e.clientX - btn.offsetLeft;
+  let y = e.clientY - btn.offsetTop;
+  let div = document.createElement("div");
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  btn.appendChild(div);
+
+  setTimeout(() => {
+    btn.removeChild(div);
+  }, 800);
+};
